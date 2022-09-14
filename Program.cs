9c1c4c8c6c4c0bab5cbc48dc45feb41bf88eff2c
Юленька <chervyak_9@mail.ru>
2022-09-14 22:31:11 +0300

    решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+// метод, который выводит одномерный массив
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1) Console.Write(" , ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+// метод, который создает новый массив заполненый элементами длина которых не превышает 3 символа
+string[] ThreeSymbol(string[] array)
+{
+    string text2 = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4 && array[i].Length > 0)
+        {
+            text2 += array[i] + " ";
+        }
+    }
+    text2 = text2.Trim();
+    string[] array2 = text2.Split();
+    return array2;
+}
+
+// код
+Console.WriteLine("Введите текст, который будет преобразован в массив. Каждое слово будет элементом массива:");
+string text = Convert.ToString(Console.ReadLine()) + "";                      // защита, на случай если пользователь ничего не введет, просто нажмет enter
+string[] arr = text.Split();
+Console.WriteLine("Исходный массив: ");
+PrintArray(arr);
+string[] arr2 = ThreeSymbol(arr);
+Console.WriteLine("Массив, состоящий из элементов исходного массива, длина которых не превышает 3 символа: ");
+PrintArray(arr2);
+
+
